0cc2a214da27d5101501257a0bb0c45266af7215
Sanskriti <bhairavi.sanskriti@gmail.com>
2023-02-25 18:37:42 +0530

    Update main.go

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -17,14 +19,14 @@ var namespace string
 var kubeconfig string
 
 var rootCmd = &cobra.Command{
-	Use:   "deployment-data",
+	Use:   "k8s-info",
 	Short: "Retrieve deployment data from a Kubernetes cluster",
 	Run:   run,
 }
 
 func init() {
 	rootCmd.PersistentFlags().StringVarP(&namespace, "namespace", "n", "default", "the namespace to retrieve deployments from")
-	rootCmd.PersistentFlags().StringVarP(&kubeconfig, "kubeconfig", "k", "./kubeconfig", "absolute path to the kubeconfig file")
+	rootCmd.PersistentFlags().StringVarP(&kubeconfig, "kubeconfig", "k", "", "absolute path to the kubeconfig file")
 }
 
 type DeploymentData struct {
@@ -81,8 +83,23 @@ func run(cmd *cobra.Command, args []string) {
 func getClientConfig(kubeconfig string) (*rest.Config, error) {
 	if kubeconfig != "" {
 		return clientcmd.BuildConfigFromFlags("", kubeconfig)
-	} else {
-		// set KUBERNETES_SERVICE_HOST and KUBERNETES_SERVICE_PORT in case the function uses in-cluster info
-		return rest.InClusterConfig()
 	}
+	//check config file path in $KUBECONFIG variable
+	kubeconfig = os.Getenv("KUBECONFIG")
+	if kubeconfig != "" {
+		return clientcmd.BuildConfigFromFlags("", kubeconfig)
+	}
+	//check config file in ~/.kube/config
+	kubeconfig = filepath.Join(os.Getenv("HOME"), ".kube", "config")
+	if kubeconfig != "" {
+		return clientcmd.BuildConfigFromFlags("", kubeconfig)
+	}
+	//check config file in the current directory
+	kubeconfig = filepath.Join(os.Getenv("PWD"), "kubeconfig")
+	if kubeconfig != "" {
+		return clientcmd.BuildConfigFromFlags("", kubeconfig)
+	}
+	// set KUBERNETES_SERVICE_HOST and KUBERNETES_SERVICE_PORT in case the function uses in-cluster info
+	return rest.InClusterConfig()
+
 }
